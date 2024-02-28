bc84fba1660f290eaf63ddbccb89b1c05e89d58b
jonascotch <joaopbpires@gmail.com>
2024-02-28 00:39:22 +0000

    added cert

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -2,7 +2,9 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 export const admin = initializeApp({
-  credential: cert("./cert.json"),
+  credential: cert(
+    "./ctx-stability-db-firebase-adminsdk-nqgh2-3b297c94ef.json"
+  ),
 });
 
 export const db = getFirestore();
