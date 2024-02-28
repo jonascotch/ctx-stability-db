cb2f0fabcdb1ba2cd3992647359c2271b387f79b
jonascotch <joaopbpires@gmail.com>
2024-02-28 01:04:16 +0000

    Another try to deploy

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -2,9 +2,7 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 export const admin = initializeApp({
-  credential: cert(
-    "./ctx-stability-db-firebase-adminsdk-nqgh2-3b297c94ef.json"
-  ),
+  credential: cert(('./ctx-stability-db-firebase-adminsdk-nqgh2-3b297c94ef.json')),
 });
 
 export const db = getFirestore();
